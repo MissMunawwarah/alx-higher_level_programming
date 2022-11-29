38c59dbfa502f062788cc56359d03665af7acf66
MissMunawwarah <madigun39@gmail.com>
2022-11-29 15:29:13 -0800

    pop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 3-print_alphabt.py
+
 for x in ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i',
         'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r',
         's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
