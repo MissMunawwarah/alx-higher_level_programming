1a079c724b8c1dc7da1bfc26f5aba149d99d8557
MissMunawwarah <madigun39@gmail.com>
2022-11-29 15:32:55 -0800

    lol

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,6 @@
 # 3-print_alphabt.py
 
 for x in ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i',
-        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r',
-        's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+          'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r',
+          's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
     print("{}".format(x), end='')
